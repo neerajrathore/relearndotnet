45eb274b34191c302f2585f5a6a0f3854daae277
Neeraj Rathore <neeraj8x@outlook.com>
2024-11-26 12:22:19 +0530

    Add project files.

---
+++ relearn.remain/Data/RealearnDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using relearn.remain.Models.Domain;
+
+namespace relearn.remain.Data
+{
+    public class RealearnDbContext : DbContext
+    {
+        public RealearnDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
